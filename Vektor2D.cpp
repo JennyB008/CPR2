3c4c18e6c9fe53f2b9803e08a1845c20fe3d20db
JennyB008 <jennifer.brenner@web.de>
2016-12-11 11:14:46 +0100

    Update Vektor2D.cpp

--- Vektor2D.cpp
+++ Vektor2D.cpp
@@ -8,23 +8,23 @@ Vektor2D::~Vektor2D(void) {
 }
 
 Vektor2D::Vektor2D(float x, float y) {
-    m_Element[0] = x;
-    m_Element[1] = y;
+	m_Element[0] = x;
+	m_Element[1] = y;
 }
 
 void Vektor2D::kopiereIn(Vektor2D v) {
-    v.m_Zeilen = m_Zeilen;
-    v.m_Spalten = m_Spalten;
+	v.m_Zeilen = m_Zeilen;
+	v.m_Spalten = m_Spalten;
 }
 
 void Vektor2D::tausche(Vektor2D* a, Vektor2D* b)
 {
-    Vektor2D temp = *a;
-    b->kopiereIn(*a);
-    temp.kopiereIn(*b);
+	Vektor2D temp = *a;
+	b->kopiereIn(*a);
+	temp.kopiereIn(*b);
 }
 
 void Vektor2D::ausgabe() {
-    std::cout << "Vektor2D:" << std::endl;
-    Matrix::ausgabe();
+	std::cout << "Vektor2D:" << std::endl;
+	Matrix::ausgabe();
 }
