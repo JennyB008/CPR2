33e1b9f9fdb937481bfbb2a4ff8a877d5dfcf35a
JennyB008 <jennifer.brenner@web.de>
2016-12-11 11:14:22 +0100

    Update Vektor2D.h

--- Vektor2D.h
+++ Vektor2D.h
@@ -4,19 +4,19 @@
 #include "Vektor.h"
 
 class Vektor2D :
-        public Vektor {
+	public Vektor {
 public:
-    Vektor2D();
+	Vektor2D();
 
-    Vektor2D(float x, float y);
+	Vektor2D(float x, float y);
 
-    static void tausche(Vektor2D* a, Vektor2D* b);
+	static void tausche(Vektor2D* a, Vektor2D* b);
 
-    void kopiereIn(Vektor2D v);
+	void kopiereIn(Vektor2D v);
 
-    void ausgabe();
+	void ausgabe();
 
-    virtual ~Vektor2D();
+	virtual ~Vektor2D();
 };
 
 #endif
