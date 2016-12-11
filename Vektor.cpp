3f0c808815aed161b0acb03d5d5decce6d0769b7
JennyB008 <jennifer.brenner@web.de>
2016-12-11 11:12:49 +0100

    Update Vektor.cpp

--- Vektor.cpp
+++ Vektor.cpp
@@ -11,44 +11,46 @@ Vektor::~Vektor()
 
 float Vektor::betrag()
 {
-    float b = skalarprodukt(*this);
-    return sqrt(b);
+	float b = skalarprodukt(*this);
+	return sqrt(b);
 }
 
 void Vektor::addiere(Vektor v)
 {
-    for (int i = 0; i < m_Zeilen * m_Spalten; i++)
-    {
-        m_Element[i] += v.m_Element[i];
-    }
+	for (int i = 0; i < m_Zeilen * m_Spalten; i++)
+	{
+		m_Element[i] += v.m_Element[i];
+	}
 }
 
 int Vektor::skalarprodukt(Vektor v)
 {
-    int sum = 0;
-    if (v.m_Spalten == m_Spalten && v.m_Zeilen == m_Zeilen)
-        for (int i = 0; i < m_Zeilen * m_Spalten; ++i)
-            sum += m_Element[i] * v.m_Element[i];
-    else
-        return nan("NAN");
-    return sum;
+	int sum = 0;
+	if (v.m_Spalten == m_Spalten && v.m_Zeilen == m_Zeilen)
+		for (int i = 0; i < m_Zeilen * m_Spalten; ++i)
+			sum += m_Element[i] * v.m_Element[i];
+	else
+		return nan("NAN");
+	return sum;
 }
 
 double Vektor::winkel(Vektor v)
 {
-    if (m_Zeilen == v.m_Zeilen && m_Spalten == v.m_Spalten)
-    {
-        double skalarprodukt = this->skalarprodukt(v);
-        double vektor1Betrag = this->betrag();
-        double vektor2Betrag = v.betrag();
+	if (m_Zeilen == v.m_Zeilen && m_Spalten == v.m_Spalten)
+	{
+		double skalarprodukt = this->skalarprodukt(v);
+		double vektor1Betrag = this->betrag();
+		double vektor2Betrag = v.betrag();
 
-        double zwischenErgebnis = 0.0;
-        zwischenErgebnis = (vektor1Betrag * vektor2Betrag) / (vektor1Betrag * vektor2Betrag);
+		double zwischenErgebnis = 0.0;
+		zwischenErgebnis = (skalarprodukt) / (vektor1Betrag * vektor2Betrag);
 
-        double winkel = acos(zwischenErgebnis);
+		double winkel = acos(zwischenErgebnis);
 
-        return winkel;
-    } else
-        return nan("NAN");
+		winkel = (winkel * 180) / 3.141592653589793238462643;
+
+		return winkel;
+	}
+	else
+		return nan("NAN");
 }
-
