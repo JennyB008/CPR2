32c9fffdf77767ffe9ef9cc18d13c4bc89f5c8c5
JennyB008 <jennifer.brenner@web.de>
2016-12-11 11:09:53 +0100

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -4,24 +4,25 @@
 static int counter = 0;
 
 Matrix::Matrix(void) : m_Zeilen(2), m_Spalten(1) {
-    std::cout << "Standardkonstruktor wird aufgerufen." << std::endl;
-    m_Element[0] = m_Zeilen;
-    m_Element[1] = m_Spalten;
-    counter +=1;
+	std::cout << "Standardkonstruktor wird aufgerufen." << std::endl;
+	m_Element[0] = m_Zeilen;
+	m_Element[1] = m_Spalten;
+	counter += 1;
 }
 
 Matrix::~Matrix() {
-    std::cout << "Matrix ";
-    ausgabe();
-    std::cout << "wird zerstoert." << std::endl;
-    counter -=1;
+	std::cout << "Matrix ";
+	ausgabe();
+	std::cout << "wird zerstoert." << std::endl;
+	counter -= 1;
 }
 
 void Matrix::ausgabe() {
-    for (int i = 0; i < m_Zeilen * m_Spalten; i++)
-        std::cout << m_Element[i] << " ";
+	for (int i = 0; i < m_Zeilen * m_Spalten; i++)
+		std::cout << m_Element[i] << " ";
+	std::cout << std::endl;
 }
 
 int Matrix::getCounter() {
-    return counter;
+	return counter;
 }
